a1627cab349e1dc06e5753f24e532ee4fb6d1471
PETE-HEMASAIREDDY <142510963+PETE-HEMASAIREDDY@users.noreply.github.com>
2023-08-17 19:00:55 +0530

    MY INFO

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @PETE-HEMASAIREDDY
+- 👀 I’m interested in coding by using python
+- 🌱 I’m currently learning machine learning
+- 💞️ I’m looking do an internships based on python language
+- 📫 my emailid "hemasaireddybunny@gmail.com"
+
+<!---
+PETE-HEMASAIREDDY/PETE-HEMASAIREDDY is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
